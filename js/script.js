d7c7a68303a7b5c44359cea3d6e29ee9b867f450
Ismael Amorim <103977244+Ismael-Amorim@users.noreply.github.com>
2023-03-01 21:12:21 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 
-var button = document.getElementById("esconder-mostrar")
+/*var button = document.getElementById("esconder-mostrar")
 
 
 
 button.addEventListener("click", function () {
 
-    var container = document.getElementById("container")
+var container = document.getElementById("container")
 
     if (container.style.display === "none") {
         container.style.display = "block"
@@ -14,3 +14,10 @@ button.addEventListener("click", function () {
     }
 
 })
+*/
+var controleCampo = 1;
+function adicionarCampo() {
+    controleCampo++
+
+    document.getElementById('infos').insertAdjacentHTML('afterend', '<div class="infos" id="campo' + controleCampo + '"> <div class="infos" id="infos"><span>Treinos:</span><br><br><div id="treino"><label>Treino:</label><br><input name="treino" id="treino" type="text" placeholder=""></div><br><div id="exercicio"><label>Exercício:</label><br><input name="exercicio" id="exercicio" type="text" placeholder=""></div><br><div id="series"><label>Séries:</label><br><input name="series" id="series" type="text" placeholder=""></div><br><div id="repeticoes"><label>Repetições:</label><br><input name="repeticoes" id="repeticoes" type="text" placeholder=""></div><br><button class="add" type="button" onclick="adicionarCampo()">+</button><br><br></div></div>')
+}
